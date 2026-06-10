a7bc000fb6e4f1028bc0d76bf2bc95a4aa5ed1b1
Diego Loyola <diegodlx@gmail.com>
2026-06-10 16:37:18 -0300

    Atualizado nomes dos arquivos login (index) e index (resumo).

--- js/index.js
+++ js/index.js
@@ -1,69 +1,29 @@
-const cardsChamados = document.querySelector('.cards-chamados');
-const resumoChamado = document.querySelector('.dados-resumo');
-const resumoPrioridade = document.querySelector('.dados-prioridade');
-const resumoPrazo = document.querySelector('.dados-deadline');
+// temporario
+import { todosChamados } from './data.js';
+const incluirDados = document.getElementById('incluir');
+const limparDados = document.getElementById('clear');
+// temporario
 
-window.onload = function() {
-    const chamadosSalvos = JSON.parse(localStorage.getItem('chamados'));
-    if (chamadosSalvos) {
-        const chamadosAbertos = chamadosSalvos.filter(chamado => chamado.status === "Aberto");
-        addHTML(chamadosAbertos);
-        fillResumo(chamadosAbertos);
-    } else {
-        cardsChamados.innerHTML = "Nenhum chamado até o momento.";
-    }
-};
+const userLogin = document.getElementById('login-form');
+const userSelect = document.getElementById('user');
 
-const addHTML = (chamados) => {
-    cardsChamados.innerHTML = "";
-    chamados.forEach(chamado => {
-        cardsChamados.innerHTML += `
-            <div class="card-chamado ${chamado.id}">
-            <p><strong>ID:</strong> ${chamado.id}</p>
-            <p><strong>Solicitante:</strong> ${chamado.solicitante}</p>
-            <p><strong>Prioridade:</strong> ${chamado.prioridade}</p>
-            <p><strong>Problema:</strong> ${chamado.problema}</p>
-            <p><strong>Prazo:</strong> ${chamado.prazo}</p>
-            </div>
-        `;
-    });
-};
 
-const fillResumo = (chamados) => {
-    const dadosResumo = chamados.reduce((acc, chamado) => {
-        acc[chamado.status] = (acc[chamado.status] || 0) + 1;
-            return acc
-    }, {});
+userLogin.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-    resumoChamado.innerHTML = `
-    <p>Abertos: ${dadosResumo.Aberto || 0}</p>
-    <p>Fechados: ${dadosResumo.Fechado || 0}</p>
-    <p>Total: ${dadosResumo.Aberto + dadosResumo.Fechado || 0}</p>
-    `;
+    console.log(userSelect.value);
 
-    const dadosPrioridade = chamados.reduce((acc, chamado) => {
-        acc[chamado.prioridade] = (acc[chamado.prioridade] || 0) + 1;
-            return acc
-    }, {});
+    window.location.href = userSelect.value;
+});
 
-    resumoPrioridade.innerHTML = `
-    <p>Alta: ${dadosPrioridade.alta || 0}</p>
-    <p>Média: ${dadosPrioridade.media || 0}</p>
-    <p>Baixa: ${dadosPrioridade.baixa || 0}</p>
-    `;
 
-    const chamadosClassificados = chamados.filter(chamado => chamado.complexidade !== "a definir" && chamado.prazo !== "a definir");
+// temporario
+limparDados.addEventListener("click", function() {
+    localStorage.clear();
+    console.log("Dados foram apagados.");
+});
 
-    const dadosPrazo = chamadosClassificados.reduce((acc, chamado) => {
-        acc.emDia = (new Date(chamado.prazo) >= new Date()) ? (acc.emDia || 0) + 1 : acc.emDia || 0;
-        acc.atrasados = (new Date(chamado.prazo) < new Date()) ? (acc.atrasados || 0) + 1 : acc.atrasados || 0;
-        return acc
-    }, {});
-
-    resumoPrazo.innerHTML = `
-    <p>Em dia: ${dadosPrazo.emDia || 0}</p>
-    <p>Atrasados: ${dadosPrazo.atrasados || 0}</p>
-    `;
-
-    
-};
+incluirDados.addEventListener("click", function() {
+    localStorage.setItem('chamados', JSON.stringify(todosChamados));
+});
+// temporario
