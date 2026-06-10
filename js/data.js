d9579709ed2a81b6cd212f86e9ad030fbb871160
Diego Loyola <diegodlx@gmail.com>
2026-06-10 16:13:46 -0300

    Reorganização do projeto em pastas

--- js/data.js
+++ js/data.js
@@ -2,61 +2,61 @@ export const todosChamados = [
     {
         id: "20240601103033", // ano mês dia hora minuto segundo
         solicitante: "João Silva",
-        prioridade: "Alta",
+        prioridade: "alta",
         problema: "Computador não liga",
         descricao: "O computador do escritório não está ligando. Já tentei verificar a fonte de alimentação e os cabos, mas não obtive sucesso.",
         data: "2024-06-01 10:30",
         hora: "10:30:33",
         status: "Aberto",
         prazo: "2024-06-08",
-        complexidade: "Alta"
+        complexidade: "alta"
     },
     {
         id: "20240602120021", // ano mês dia hora minuto segundo
-        solicitante: "Maria Santos",
-        prioridade: "Média",
+        solicitante: "João Silva",
+        prioridade: "media",
         problema: "Problema com impressora",
         descricao: "A impressora não está imprimindo corretamente. Já tentei reiniciar o serviço e verificar a conexão, mas o problema persiste.",
         data: "2024-06-02",
         hora: "12:00:21",
         status: "Aberto",
         prazo: "2024-06-05",
-        complexidade: "Média"
+        complexidade: "media"
     },
     {
         id: "20240603150000", // ano mês dia hora minuto segundo
-        solicitante: "Carlos Oliveira",
-        prioridade: "Baixa",
+        solicitante: "João Silva",
+        prioridade: "baixa",
         problema: "Problema com mouse",
         descricao: "O mouse está com problemas de funcionamento. Já tentei limpar os sensores e verificar a conexão, mas o problema persiste.",
         data: "2024-06-03",
         hora: "15:00:00",
         status: "Aberto",
         prazo: "2024-06-04",
-        complexidade: "Baixa"
+        complexidade: "baixa"
     },
     {
         id: "20240604140045", // ano mês dia hora minuto segundo
-        solicitante: "Ana Costa",
-        prioridade: "Alta",
+        solicitante: "João Silva",
+        prioridade: "alta",
         problema: "Problema com software",
         descricao: "O software de gestão de projetos está apresentando erros frequentes. Já tentei reinstalar o software e verificar as atualizações, mas o problema persiste.",
         data: "2024-06-04",
         hora: "14:00:45",
         status: "Aberto",
         prazo: "2024-06-10",
-        complexidade: "Alta"
+        complexidade: "alta"
     },
     {
         id: "20240605110015", // ano mês dia hora minuto segundo
-        solicitante: "Pedro Almeida",
-        prioridade: "Média",
+        solicitante: "João Silva",
+        prioridade: "media",
         problema: "Problema com rede",
         descricao: "A rede está instável e apresentando quedas frequentes. Já tentei reiniciar o roteador e verificar as conexões, mas o problema persiste.",
         data: "2024-06-05",
         hora: "11:00:15",
         status: "Fechado",
         prazo: "2024-06-08",
-        complexidade: "Média"
+        complexidade: "media"
     }
 ];
---
+++ js/login.js
@@ -0,0 +1,29 @@
+// temporario
+import { todosChamados } from '../js//data.js';
+const incluirDados = document.getElementById('incluir');
+const limparDados = document.getElementById('clear');
+// temporario
+
+const userLogin = document.getElementById('login-form');
+const userSelect = document.getElementById('user');
+
+
+userLogin.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    console.log(userSelect.value);
+
+    window.location.href = userSelect.value;
+});
+
+
+// temporario
+limparDados.addEventListener("click", function() {
+    localStorage.clear();
+    console.log("Dados foram apagados.");
+});
+
+incluirDados.addEventListener("click", function() {
+    localStorage.setItem('chamados', JSON.stringify(todosChamados));
+});
+// temporario
