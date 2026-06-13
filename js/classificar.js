97378d9bcfe8a70faf2db043c43600af063d3da2
Diego Loyola <diegodlx@gmail.com>
2026-06-12 21:04:44 -0300

    Implementado <dialog> de mais detalhes e classificação ao clicar no card.

--- js/classificar.js
+++ js/classificar.js
@@ -1,4 +1,7 @@
 const cardsChamados = document.querySelector('.cards-chamados');
+const modal = document.getElementById('modal-chamado');
+const modalContent = document.getElementById('modal-content');
+const btnFechar = document.getElementById('btn-fechar');
 
 window.onload = function() {
     const chamadosSalvos = JSON.parse(localStorage.getItem('chamados'));
@@ -6,7 +9,7 @@ window.onload = function() {
         const chamadosAbertos = chamadosSalvos.filter(chamado => chamado.complexidade === "a definir");
         addHTML(chamadosAbertos);
     } else {
-        cardsChamados.innerHTML = "Nenhum chamado até o momento.";
+        cardsChamados.innerHTML = "Nenhum chamado para classificar até o momento.";
     }
 };
 
@@ -14,7 +17,7 @@ const addHTML = (chamados) => {
     cardsChamados.innerHTML = "";
     chamados.forEach(chamado => {
         cardsChamados.innerHTML += `
-            <div class="card-chamado ${chamado.id}">
+            <div class="card-chamado" id="${chamado.id}">
             <p><strong>ID:</strong> ${chamado.id}</p>
             <p><strong>Solicitante:</strong> ${chamado.solicitante}</p>
             <p><strong>Prioridade:</strong> ${chamado.prioridade}</p>
@@ -23,4 +26,76 @@ const addHTML = (chamados) => {
             </div>
         `;
     });
+};
+
+cardsChamados.addEventListener("click", (e) => {
+    const id = e.target.closest(".card-chamado").id;
+    const chamadosSalvos = JSON.parse(localStorage.getItem('chamados'));
+    const chamadoModal = chamadosSalvos.filter(chamado => chamado.id === id)[0];
+    modalContent.innerHTML = `
+        <div class="modal" id="${id}">
+        <h3>Detalhes do chamado</h3>
+        <p><strong>ID:</strong> ${chamadoModal.id}</p>
+        <p><strong>Solicitante:</strong> ${chamadoModal.solicitante}</p>
+        <p><strong>Abertura:</strong> ${chamadoModal.data}</p>
+        <p><strong>Problema:</strong> ${chamadoModal.problema}</p>
+        <p><strong>Prioridade:</strong> ${chamadoModal.prioridade}</p>
+        <p><strong>Descrição:</strong> ${chamadoModal.descricao}</p>
+        <label for="complexidade"><strong>Classificar complexidade:</strong></label>
+        <select name="complexidade" id="complexidade">
+            <option value="baixa">Baixa</option>
+            <option value="media">Média</option>
+            <option value="alta">Alta</option>
+        </select>
+        <button id="btn-salvar">Salvar</button>
+        <button id="btn-fechar">Fechar</button>
+        </div>
+    `;
+
+    const btnFechar = document.getElementById('btn-fechar');
+    const btnSalvar = document.getElementById('btn-salvar');
+    const complexidade = document.getElementById('complexidade');
+
+    btnFechar.addEventListener('click', () => {
+    modal.close();
+    });
+
+    btnSalvar.addEventListener('click', () => {
+        salvarChamado(id, complexidade.value);
+    });
+
+    modal.showModal();
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.close();
+  }
+});
+
+const salvarChamado = (id, complexidade) => {
+    const chamadosSalvos = JSON.parse(localStorage.getItem('chamados'));
+    const indexChamado = chamadosSalvos.findIndex(chamado => chamado.id === id);
+    
+    let prazoDias = 0;
+    if (complexidade === "baixa") {
+        prazoDias = 1;
+    } else if (complexidade === "media") {
+        prazoDias = 3;
+    } else if (complexidade === "alta") {
+        prazoDias = 7;
+    };
+
+    if (indexChamado !== -1) {
+        const dataAbertura = new Date(chamadosSalvos[indexChamado].data + "T12:00:00");
+        dataAbertura.setDate(dataAbertura.getDate() + prazoDias);
+        const prazo = dataAbertura.toISOString().split('T')[0];
+        chamadosSalvos[indexChamado].complexidade = complexidade;
+        chamadosSalvos[indexChamado].status = "Atendimento";
+        chamadosSalvos[indexChamado].prazo = prazo;
+        localStorage.setItem('chamados', JSON.stringify(chamadosSalvos));
+        modal.close();
+        const chamadosAbertos = chamadosSalvos.filter(chamado => chamado.complexidade === "a definir");
+        addHTML(chamadosAbertos);
+    };
 };
